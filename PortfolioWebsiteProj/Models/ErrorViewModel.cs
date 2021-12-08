3a3b42e1f742dd1a0acdafb24c0d1411a815c03d
Jonah Ringdahl <Jonahringdahl@outlook.com>
2021-12-07 21:38:38 -0500

    Github was mad
    Github did not like the presence of the live website along the static website

--- PortfolioWebsiteProj/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PortfolioWebsiteProj.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
